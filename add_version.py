9bef78066b72d78dfdc17651000e7221986c6c93
Azzam <azzam.zafar21@gmail.com>
2022-12-05 14:01:48 +0530

    parse extras of a package mentioned inside square brackets

--- add_version.py
+++ add_version.py
@@ -7,11 +7,31 @@ with open('./temp','r') as versionfile:
         ...
         package,version = line.split('==')
         packages[package]=version
+
+def parse_extras(parts):
+    """ parse package extras present inside
+        square brackets.
+    """
+    for part in parts:
+        if ']' in part:
+            extras = part.split(']')[0]
+            extra_list = extras.split(',')
+            for val in extra_list:
+                if val!='':
+                    return re.split('==|>=|>|<=|<',val)[0]
+                    
 listings=[]
 with open('./requirements.txt','r') as requirements:
     for line in requirements.readlines():
-        listing = re.split(r'\n|==|<=|<|>=|>',line)[0]
-        listings.append(listing) 
+        parts = line.split('[')
+        if len(parts)==1:
+            listing = re.split(r'\n|==|<=|<|>=|>',line)[0]
+            listings.append(listing)
+        else:
+            # print(parts[0])
+            listings.append(parts[0])
+            package=parse_extras(parts)
+            listings.append(package)
 
 for package in packages.keys():
     if package not in listings:
@@ -21,5 +41,6 @@ with open('./newreq','a+') as newreq:
     for key,val in packages.items():
         if val!=None:
             newreq.write(f'{key}=={val}')
+os.system('rm temp && mv ./newreq ./requirements.txt --force')
 
 
